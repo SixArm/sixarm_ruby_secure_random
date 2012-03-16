500a09fb0bb01510b0e79b65e13ce6e0e96a0792
SixArm <sixarm@sixarm.com>
2012-03-16 09:08:37 -0700

    Upgrade from test/unit to minitest/autorun

--- test/sixarm_ruby_secure_random_test.rb
+++ test/sixarm_ruby_secure_random_test.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+require 'minitest/autorun'
 require 'simplecov'
 SimpleCov.start
-require 'test/unit'
 require 'sixarm_ruby_secure_random'
 
 class Testing < Test::Unit::TestCase
