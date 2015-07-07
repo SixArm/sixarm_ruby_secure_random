69f8e4c45cc2ec92e1cdbdb86f82570352216a8e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:54 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_secure_random_test.rb
+++ test/sixarm_ruby_secure_random_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_secure_random'
+require "sixarm_ruby_secure_random"
 
 class Testing < Test::Unit::TestCase
 
